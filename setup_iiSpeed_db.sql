af547ced47901828767b6f9be471eed3ab388d41
Gavin Eke <GavinEke@users.noreply.github.com>
2017-02-22 10:10:08 +1100

    Update setup_iiSpeed_db.sql

--- setup_iiSpeed_db.sql
+++ setup_iiSpeed_db.sql
@@ -1,8 +1,6 @@
 CREATE DATABASE iiSpeed;
 CREATE USER 'iiSpeed'@'localhost' IDENTIFIED BY 'password';
 GRANT ALL PRIVILEGES ON iiSpeed.* TO 'iiSpeed'@'localhost';
-CREATE USER 'iiSpeed'@'localhost' IDENTIFIED BY 'password';
-GRANT SELECT ON iiSpeed.* TO 'iiSpeed'@'localhost';
 FLUSH PRIVILEGES;
 USE iiSpeed;
 CREATE TABLE data ( Date varchar(255), Ping varchar(255), DownSpeed varchar(255), UpSpeed varchar(255) );
